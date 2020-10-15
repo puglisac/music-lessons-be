c25901301d85d77938b5ab2ea3979a17a0fd232c
Alan Puglisi <alan.c.puglisi@gmail.com>
2020-10-15 17:03:33 -0500

    updated teacher model

--- models/teacher.js
+++ models/teacher.js
@@ -4,14 +4,15 @@ const db = require("../db");
 const ExpressError = require("../helpers/expressError");
 
 class Teacher {
-	constructor(username, full_name, email) {
+	constructor(username, full_name, email, students = []) {
 		this.username = username;
 		this.full_name = full_name;
 		this.email = email;
+		this.students = students;
 
 	}
 
-	static async register(username, password, full_name, email,) {
+	static async register(username, password, full_name, email) {
 		const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
 		const result = await db.query(
 			`INSERT INTO teachers (
@@ -44,15 +45,24 @@ class Teacher {
 
 	static async get(username) {
 		const teacher = await db.query(
-			`SELECT username, full_name, email FROM teachers WHERE username = $1`,
+			`SELECT t.username, t.full_name, t.email, 
+			s.username AS student_username, s.full_name 
+			AS student_full_name, 
+			s.email AS student_email
+			FROM teachers AS t FULL JOIN students AS s 
+			ON s.teacher_username = t.username 
+			WHERE username = $1`,
 			[username]
 		);
 		if (!teacher.rows[0]) {
 			throw new ExpressError(`No such user: ${username}`, 404);
 		}
 		const t = teacher.rows[0];
+		const students = teacher.rows.map(s => [s.student_username,
+		s.student_full_name,
+		s.student_email]);
 
-		return new Teacher(t.username, t.full_name, t.email);
+		return new Teacher(t.username, t.full_name, t.email, students);
 	}
 	async save() {
 		await db.query(
